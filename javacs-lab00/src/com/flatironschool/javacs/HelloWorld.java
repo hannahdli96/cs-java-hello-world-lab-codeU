b5b7ecf568f8e8b39caf9cba1eb57333f8b02b6c
Hannah Li <hannahli@berkeley.edu>
2016-03-21 20:49:03 -0400

    Done.

--- javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorld.java
@@ -3,7 +3,7 @@ package com.flatironschool.javacs;
 public class HelloWorld {
     public static Double getVersion() {
         // I won't work!
-        String s = System.getProperty("java.version");
+        String s = System.getProperty("java.specification.version");
         return Double.parseDouble(s);
     }
 
